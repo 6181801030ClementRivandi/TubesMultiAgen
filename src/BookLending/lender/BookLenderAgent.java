f7d6430fafdbb79872dfa52e316af9ceefc0893e
clement rivandi <clement.rivandi.cr@gmail.com>
2021-07-09 13:55:56 +0700

    lender GUI

--- src/BookLending/lender/BookLenderAgent.java
+++ src/BookLending/lender/BookLenderAgent.java
@@ -1,15 +1,145 @@
 package BookLending.lender;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.TickerBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
-/**
- *
- * @author user
- */
-public class BookLenderAgent {
-    
-}
+public class BookLenderAgent extends Agent {  
+  // katalog buku
+  private Map catalogue = new HashMap();  
+  
+  // Gui 
+  private BookLenderGui myGui;  
+  
+  protected void setup() {  
+
+    System.out.println("Lender-agent "+getAID().getName()+" is ready.");  
+  
+    // membuat dan menampilkan GUI  
+    myGui = new BookLenderGuiImpl();  
+    myGui.setAgent(this);  
+    myGui.show();  
+      
+    // menambah behaviour
+    addBehaviour(new CallForOfferServer());  
+  
+    DFAgentDescription dfd = new DFAgentDescription();  
+    dfd.setName(getAID());  
+    ServiceDescription sd = new ServiceDescription();  
+    sd.setType("Book-lending");  
+    sd.setName(getLocalName()+"-Book-lending");  
+    dfd.addServices(sd);  
+    try {  
+      DFService.register(this, dfd);  
+    }  
+    catch (FIPAException fe) {  
+      fe.printStackTrace();  
+    }  
+  }  
+  
+  protected void takeDown() {  
+    if (myGui != null) {  
+      myGui.dispose();  
+    }  
+  
+    System.out.println("lender-agent "+getAID().getName()+"terminating.");  
+  
+    try {  
+      DFService.deregister(this);  
+    }  
+    catch (FIPAException fe) {  
+      fe.printStackTrace();  
+    }  
+  }  
+
+  public void putForSale(String title, int initPrice, int minPrice, Date deadline) {  
+    addBehaviour(new PriceManager(this, title, initPrice, minPrice, deadline));  
+  }  
+  
+  
+  
+  private class PriceManager extends TickerBehaviour {  
+    private String title;  
+    private int minPrice, currentPrice, initPrice, deltaP;  
+    private long initTime, deadline, deltaT;  
+  
+    private PriceManager(Agent a, String t, int ip, int mp, Date d) {  
+      super(a, 60000); // tick every minute  
+      title = t;  
+      initPrice = ip;  
+      currentPrice = initPrice;  
+      deltaP = initPrice - mp;  
+      deadline = d.getTime();  
+      initTime = System.currentTimeMillis();  
+      deltaT = ((deadline - initTime) > 0 ? (deadline - initTime) : 60000);  
+    }  
+  
+    public void onStart() {  
+      // Insert the book in the catalogue of books available for sale  
+      catalogue.put(title, this);  
+      super.onStart();  
+    }  
+  
+    public void onTick() {  
+      long currentTime = System.currentTimeMillis();  
+      if (currentTime > deadline) {  
+        // Deadline expired  
+        myGui.notifyUser("Cannot sell book "+title);  
+        catalogue.remove(title);  
+        stop();  
+      }  
+      else {  
+        // Compute the current price  
+        long elapsedTime = currentTime - initTime;  
+        currentPrice = (int)Math.round(initPrice - 1.0 * deltaP * (1.0 * elapsedTime / deltaT));  
+      }  
+    }  
+  
+    public int getCurrentPrice() {  
+      return currentPrice;  
+    }  
+  }  
+ 
+  private class CallForOfferServer extends CyclicBehaviour {  
+    private MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);  
+  
+    public void action() {  
+      ACLMessage msg = myAgent.receive(mt);  
+      if (msg != null) {  
+        // CFP Message received. Process it  
+        String title = msg.getContent();  
+        myGui.notifyUser("Received Proposal to buy "+title);  
+        ACLMessage reply = msg.createReply();  
+        PriceManager pm = (PriceManager) catalogue.get(title);  
+        if (pm != null) {  
+          // The requested book is available for sale. Reply with the price  
+          reply.setPerformative(ACLMessage.PROPOSE);  
+          reply.setContent(String.valueOf(pm.getCurrentPrice()));  
+        }  
+        else {  
+          // The requested book is NOT available for sale.  
+          reply.setPerformative(ACLMessage.REFUSE);  
+        }  
+        myAgent.send(reply);  
+        myGui.notifyUser(pm != null ? "Sent Proposal to sell at "+reply.getContent() : "Refused Proposal as the book is not for sale");  
+      }  
+      else {  
+        block();  
+      }  
+    }  
+  } // End of inner class CallForOfferServer  
+  
+  
+      
+  
+  
+}  
--- src/BookLending/lender/BookLenderGuiImpl.java
+++ src/BookLending/lender/BookLenderGuiImpl.java
@@ -5,10 +5,227 @@
  */
 package BookLending.lender;
 
-/**
- *
- * @author ASUS
- */
-public class BookLenderGuiImpl {
+import jade.gui.TimeChooser;   
+   
+import java.awt.*;   
+import java.awt.event.*;   
+import javax.swing.*;   
+import javax.swing.border.*;   
+   
+import java.util.Date;   
     
-}
+public class BookLenderGuiImpl extends JFrame implements BookLenderGui {   
+    private BookLenderAgent myAgent;   
+       
+    private JTextField titleTF, maxCostTF, minCostTF, deadlineTF;   
+    private JButton setDeadlineB, setExec;   
+    private JTextArea logTA;   
+    private Date deadline;   
+       
+    public BookLenderGuiImpl() {   
+        super();   
+           
+        addWindowListener(new   WindowAdapter() {   
+            public void windowClosing(WindowEvent e) {   
+                myAgent.doDelete();   
+            }   
+        } );   
+
+        JPanel rootPanel = new JPanel();   
+        rootPanel.setLayout(new GridBagLayout());   
+    rootPanel.setMinimumSize(new Dimension(330, 125));   
+    rootPanel.setPreferredSize(new Dimension(330, 125));   
+           
+    ///////////   
+    // Line 0   
+    ///////////   
+        JLabel l = new JLabel("Judul");   
+    l.setHorizontalAlignment(SwingConstants.LEFT);   
+    GridBagConstraints gridBagConstraints = new GridBagConstraints();   
+    gridBagConstraints.gridx = 0;   
+    gridBagConstraints.gridy = 0;   
+    gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;   
+    gridBagConstraints.insets = new java.awt.Insets(5, 3, 0, 3);   
+    rootPanel.add(l, gridBagConstraints);   
+   
+    titleTF = new JTextField(64);   
+    titleTF.setMinimumSize(new Dimension(210, 20));   
+    titleTF.setPreferredSize(new Dimension(210, 20));   
+    gridBagConstraints = new GridBagConstraints();   
+    gridBagConstraints.gridx = 1;   
+    gridBagConstraints.gridy = 0;   
+    gridBagConstraints.gridwidth = 3;   
+    gridBagConstraints.anchor = GridBagConstraints.NORTHWEST;   
+    gridBagConstraints.insets = new Insets(5, 3, 0, 3);   
+    rootPanel.add(titleTF, gridBagConstraints);   
+   
+    ///////////   
+    // Line 1   
+    ///////////    
+        l = new JLabel("Harga Maksimal");   
+    l.setHorizontalAlignment(SwingConstants.LEFT);   
+    l.setMinimumSize(new Dimension(100, 20));   
+    l.setPreferredSize(new Dimension(100, 20));   
+    gridBagConstraints = new GridBagConstraints();   
+    gridBagConstraints.gridx = 0;   
+    gridBagConstraints.gridy = 1;   
+    gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;   
+    gridBagConstraints.insets = new java.awt.Insets(5, 3, 0, 3);   
+    rootPanel.add(l, gridBagConstraints); 
+    
+    maxCostTF = new JTextField(64);   
+    maxCostTF.setMinimumSize(new Dimension(80, 20));   
+    maxCostTF.setPreferredSize(new Dimension(80, 20));   
+    gridBagConstraints = new GridBagConstraints();   
+    gridBagConstraints.gridx = 1;   
+    gridBagConstraints.gridy = 1;   
+    gridBagConstraints.anchor = GridBagConstraints.NORTHWEST;   
+    gridBagConstraints.insets = new Insets(5, 3, 0, 3);   
+    rootPanel.add(maxCostTF, gridBagConstraints);
+    
+    ///////////   
+    // Line 2   
+    ///////////    
+        l = new JLabel("Harga Minimal");   
+    l.setHorizontalAlignment(SwingConstants.LEFT);   
+    l.setMinimumSize(new Dimension(100, 20));   
+    l.setPreferredSize(new Dimension(100, 20));   
+    gridBagConstraints = new GridBagConstraints();   
+    gridBagConstraints.gridx = 0;   
+    gridBagConstraints.gridy = 2;   
+    gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;   
+    gridBagConstraints.insets = new java.awt.Insets(5, 3, 0, 3);   
+    rootPanel.add(l, gridBagConstraints); 
+    
+    minCostTF = new JTextField(64);   
+    minCostTF.setMinimumSize(new Dimension(80, 20));   
+    minCostTF.setPreferredSize(new Dimension(80, 20));   
+    gridBagConstraints = new GridBagConstraints();   
+    gridBagConstraints.gridx = 1;   
+    gridBagConstraints.gridy = 2;   
+    gridBagConstraints.anchor = GridBagConstraints.NORTHWEST;   
+    gridBagConstraints.insets = new Insets(5, 3, 0, 3);   
+    rootPanel.add(minCostTF, gridBagConstraints); 
+    
+    ///////////   
+    // Line 3  
+    ///////////   
+        l = new JLabel("Batas waktu");   
+    l.setHorizontalAlignment(SwingConstants.LEFT);   
+    gridBagConstraints = new GridBagConstraints();   
+    gridBagConstraints.gridx = 0;   
+    gridBagConstraints.gridy = 3;   
+    gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;   
+    gridBagConstraints.insets = new java.awt.Insets(5, 3, 0, 3);   
+    rootPanel.add(l, gridBagConstraints);   
+   
+    deadlineTF = new JTextField(64);   
+    deadlineTF.setMinimumSize(new Dimension(146, 20));   
+    deadlineTF.setPreferredSize(new Dimension(146, 20));   
+    deadlineTF.setEnabled(false);   
+    gridBagConstraints = new GridBagConstraints();   
+    gridBagConstraints.gridx = 1;   
+    gridBagConstraints.gridy = 3;   
+    gridBagConstraints.gridwidth = 2;   
+    gridBagConstraints.anchor = GridBagConstraints.NORTHWEST;   
+    gridBagConstraints.insets = new Insets(5, 3, 0, 3);   
+    rootPanel.add(deadlineTF, gridBagConstraints);   
+   
+    setDeadlineB = new JButton("Set");   
+    setDeadlineB.setMinimumSize(new Dimension(70, 20));   
+    setDeadlineB.setPreferredSize(new Dimension(70, 20));   
+        setDeadlineB.addActionListener(new ActionListener(){   
+        public void actionPerformed(ActionEvent e) {   
+            Date d = deadline;   
+            if (d == null) {   
+                d = new Date();   
+            }   
+            TimeChooser tc = new TimeChooser(d);   
+            if (tc.showEditTimeDlg(BookLenderGuiImpl.this) == TimeChooser.OK) {   
+                deadline = tc.getDate();   
+                deadlineTF.setText(deadline.toString());   
+            }   
+        }   
+        } );   
+    gridBagConstraints = new GridBagConstraints();   
+    gridBagConstraints.gridx = 3;   
+    gridBagConstraints.gridy = 3;   
+    gridBagConstraints.anchor = GridBagConstraints.NORTHWEST;   
+    gridBagConstraints.insets = new Insets(5, 3, 0, 3);   
+    rootPanel.add(setDeadlineB, gridBagConstraints);     
+           
+    rootPanel.setBorder(new BevelBorder(BevelBorder.LOWERED));   
+       
+    getContentPane().add(rootPanel, BorderLayout.NORTH);   
+       
+       
+    logTA = new JTextArea();   
+    logTA.setEnabled(false);   
+    JScrollPane jsp = new JScrollPane(logTA);   
+    jsp.setMinimumSize(new Dimension(300, 180));   
+    jsp.setPreferredSize(new Dimension(300, 180));   
+    JPanel p = new JPanel();   
+    p.setBorder(new BevelBorder(BevelBorder.LOWERED));   
+    p.add(jsp);   
+    getContentPane().add(p, BorderLayout.CENTER);   
+     
+       
+    p = new JPanel();   
+    setExec = new JButton("Execute Order");   
+        setExec.addActionListener(new ActionListener(){   
+        public void actionPerformed(ActionEvent e) {   
+            String title = titleTF.getText();    
+            int maxCost = -1;              
+            if (title != null && title.length() > 0) {   
+                if (deadline != null && deadline.getTime() > System.currentTimeMillis()) {   
+                    try {   
+                        //desiredCost = Integer.parseInt(desiredCostTF.getText());   
+                        try {   
+                            maxCost = Integer.parseInt(maxCostTF.getText());   
+                            // if (maxCost >= desiredCost) {   
+                                // myAgent.purchase(title, desiredCost, maxCost, deadline.getTime());   
+                                //myAgent.purchase(title, maxCost, deadline);   
+                  notifyUser("PUT FOR LEND: "+title+" at max "+maxCost+" by "+deadline);    
+                            //}   
+                            //else {   
+                                // Max cost < desiredCost   
+                                //JOptionPane.showMessageDialog(BookBuyerGuiImpl.this, "Max cost must be greater than best cost", "WARNING", JOptionPane.WARNING_MESSAGE);   
+                            //}                                
+                        }   
+                        catch (Exception ex1) {   
+                            // Invalid max cost   
+                            JOptionPane.showMessageDialog(BookLenderGuiImpl.this, "Invalid max cost", "WARNING", JOptionPane.WARNING_MESSAGE);   
+                        }   
+                    }   
+                    catch (Exception ex2) {   
+                        // No deadline specified   
+                        JOptionPane.showMessageDialog(BookLenderGuiImpl.this, "Invalid deadline", "WARNING", JOptionPane.WARNING_MESSAGE); 
+                    }   
+                }   
+            }   
+            else {   
+                // No book title specified   
+                JOptionPane.showMessageDialog(BookLenderGuiImpl.this, "No book title specified", "WARNING", JOptionPane.WARNING_MESSAGE);   
+            }   
+        }   
+        } );    
+       
+    p.add(setExec);    
+       
+    p.setBorder(new BevelBorder(BevelBorder.LOWERED));   
+    getContentPane().add(p, BorderLayout.SOUTH);   
+       
+    pack();   
+       
+    setResizable(false);   
+    }   
+   
+    public void setAgent(BookLenderAgent a) {   
+        myAgent = a;   
+        setTitle(myAgent.getName());   
+    }   
+           
+    public void notifyUser(String message) {   
+        logTA.append(message+"\n");   
+    }   
+}     
